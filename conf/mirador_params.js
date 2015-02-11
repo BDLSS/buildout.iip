38171d84d221668f576cbadfc3a15f35eb7616d0
calvin butcher <calvin.butcher@bodleian.ox.ac.uk>
2015-02-11 17:05:21 +0000

    modified apache mod fcgi url changed

---
+++ conf/mirador_params.js
@@ -0,0 +1,40 @@
+      Mirador({
+        "id": "viewer",
+        "data": [{
+          "manifestUri": "data/Walters/bd183mz0176/Manifest.json",
+          "title": "MS 530",
+          "widgets": [{
+            "height": 500,
+            "openAt": "fol. W.688a",
+            "type": "imageView",
+            "width": 500
+          }, {
+            "height": 250,
+            "type": "thumbnailsView",
+            "width": 500
+          }]
+        },
+        { "manifestUri": "data/Walters/zp295gk6974/manifest.json", "location": "Stanford University", "title": "MS 5", "widgets": [] },
+        { "manifestUri": "data/Walters/rn216th9534/manifest.json", "location": "Stanford University", "title": "W.788", "widgets": [] },
+        { "manifestUri": "data/Walters/zw200wd8767/manifest.json", "location": "Stanford University", "title": "W.788", "widgets": [] },
+        { "manifestUri": "data/Walters/yq907xc9138/manifest.json", "location": "Stanford University", "title": "W.788", "widgets": [] },
+        { "manifestUri": "http://dms-data.stanford.edu/data/manifests/Walters/qm670kv1873/manifest.json", "location": "Stanford University", "title": "W.168", "widgets": [] },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/Admont23.json", "location": "Yale University", "title": "Admont 23", "widgets": [] },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/Admont43.json", "location": "Yale University", "title": "Admont 43", "widgets": [] },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/BeineckeMS10.json", "location": "Yale University", "title": "Beinecke MS 10", "widgets": [] },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/BeineckeMS109.json", "location": "Yale University", "title": "Beinecke MS 109", "widgets": [] },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/BeineckeMS310.json", "location": "Yale University", "title": "Beinecke MS 310", "widgets": [{
+            "height": 500,
+            "type": "thumbnailsView",
+            "width": 400
+          }]
+        },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/BeineckeMS360.json", "location": "Yale University", "title": "Beinecke MS 360", "widgets": [] },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/BeineckeMS525.json", "location": "Yale University", "title": "Beinecke MS 525", "widgets": [] },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/BeineckeMS748.json", "location": "Yale University", "title": "Beinecke MS 748", "widgets": [] },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/BeineckeMS758_5.json", "title": "Beinecke MS 758.5", "widgets": [] },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/BodleianMSBodley113.json", "location": "Yale University", "title": "Bodleian MS Bodley 113", "widgets": [] },
+        { "manifestUri": "http://www.shared-canvas.org/impl/demo1d/res/manifest.json", "location": "Shared Canvas", "title": "Pierpont Morgan MS.804", "widgets": [  { "height": 500, "type": "thumbnailsView", "width": 500 } ] },
+        { "manifestUri": "http://manifests.ydc2.yale.edu/manifest/Osbornfa1.json", "location": "Yale University", "title": "Osborn fa1", "widgets": [] },
+        ]
+      });
